4c687718fad56d62a2565580183a4026dba18089
Стачинский Никита <stachinskiy.nikita@gmail.com>
2024-12-14 19:01:44 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            initDriveButtons(LeftTableDisks);
+            initDriveButtons(RightTableDisks);
+
+            initTable(LeftTable, @"C:\");
+            initTable(RightTable, @"C:\");
+        }
+
+        public void initTable(DataGrid table, string path)
+        {
+            List<ComplexData> complexData = new List<ComplexData>();
+            TableData tableData = new TableData();
+            bool isError = tableData.determineFiles(path);
+
+            if (!isError)
+            {
+                for (int i = 0; i < tableData.getDirCount(); i++)
+                    complexData.Add(new ComplexData(tableData.getDir(i)));
+                for (int i = 0; i < tableData.getFileCount(); i++)
+                    complexData.Add(new ComplexData(tableData.getFile(i)));
+
+                table.ItemsSource = complexData;
+
+                if (table.Name[0] == 'L')
+                    LeftTablePath.Text = path;
+                else
+                    RightTablePath.Text = path;
+            }    
+        }
+
+        public void initDriveButtons(WrapPanel wrapPanel)
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            for (int i = 0; i < drives.Length; i++)
+            {
+                Button diskButton = new Button() { Content = drives[i] };
+                diskButton.Click += diskButton_Click;
+                wrapPanel.Children.Add(diskButton);
+            }
+        }
+
+        public void diskButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button senderButton = sender as Button;
+            string senderName = senderButton.Content.ToString();
+
+            WrapPanel senderParent = senderButton.Parent as WrapPanel;
+            string senderParentName = senderParent.Name;
+            if (senderParentName[0] == 'L')
+                initTable(LeftTable, senderName);
+            else
+                initTable(RightTable, senderName);
+        }
+        private void Row_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            DataGridRow row = sender as DataGridRow;
+            // Some operations with this row
+        }
+    }
+}
---
+++ Table.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+
+
+namespace WpfApp2
+{
+    public class ComplexData
+    {
+        public ComplexData(File file)
+        {
+            this.name = file.name;
+            this.type = file.type;
+            this.dateCreated = file.dateCreated;
+            this.dateLastWrite = file.dateLastWrite;
+        }
+        
+        public ComplexData(Dir dir)
+        {
+            this.name = dir.name;
+            this.type = dir.type;
+        }
+
+        public string name { get; set; }
+        public string type { get; set; }
+        public DateTime dateCreated { get; set; }
+        public DateTime dateLastWrite { get; set; }
+    }
+
+    public class TableData
+    {
+        public bool determineFiles(string path)
+        {
+            try
+            {
+                string[] allFiles = Directory.GetFiles(path);
+                files = new File[allFiles.Length];
+
+                string[] allDirectories = Directory.GetDirectories(path);
+                dirs = new Dir[allDirectories.Length];
+
+                for (int i = 0; i < allFiles.Length; i++)
+                    files[i] = new File(allFiles[i]);
+
+                for (int i = 0; i < allDirectories.Length; i++)
+                    dirs[i] = new Dir(allDirectories[i]);
+            }
+            catch (Exception e)
+            {
+                string messageBoxText = "Error while loading file";
+                string caption = "Error";
+                MessageBoxButton button = MessageBoxButton.YesNoCancel;
+                MessageBoxImage icon = MessageBoxImage.Warning;
+
+                MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.Yes);
+
+                return true;
+            }
+            return false;
+        }
+
+        public int getDataCount()
+        {
+            return files.Length + dirs.Length;
+        }
+
+        public int getFileCount()
+        {
+            return files.Length;
+        }
+
+        public int getDirCount()
+        {
+            return dirs.Length;
+        }
+
+        public File getFile(int index)
+        {
+            return files[index];
+        }
+        public Dir getDir(int index)
+        {
+            return dirs[index];
+        }
+
+        private string path;
+        File[] files;
+        Dir[] dirs;
+    }
+    public class File
+    {
+        public File(string fullFilename)
+        {
+            int indLastSlash = fullFilename.LastIndexOf('/');
+            int indDot = fullFilename.LastIndexOf('.');
+
+            string fileName = "";
+            for (int i = indLastSlash + 1; i < indDot; i++)
+                fileName += fullFilename[i];
+
+            string fileType = "";
+            for (int i = indDot + 1; i < fullFilename.Length; i++)
+                fileType += fullFilename[i];
+
+            this.name = fileName;
+            this.type = fileType;
+
+            FileInfo fileInfo = new FileInfo(fullFilename);
+            this.dateCreated = fileInfo.CreationTime;
+            this.dateLastWrite = fileInfo.LastWriteTime;
+        }
+
+        public string name { get; set; }
+        public string type { get; set; }
+        public DateTime dateCreated { get; set; }
+        public DateTime dateLastWrite { get; set; }
+    }
+
+    public class Dir
+    {
+        public Dir(string fullDirectoryName)
+        {
+            int indLastSlash = fullDirectoryName.LastIndexOf('/');
+
+            string directoryName = "";
+            for (int i = indLastSlash + 1; i < fullDirectoryName.Length; i++)
+                directoryName += fullDirectoryName[i];
+
+            this.name = directoryName;
+        }
+
+        public string name { get; set; }
+        public string type { get; set; }
+        public DateTime date { get; set; }
+    }
+}
